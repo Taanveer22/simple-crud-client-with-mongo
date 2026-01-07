fa2bc9dda17e295d7e1733d21918c113daf855a0
Taanveer22 <taanveer469@gmail.com>
2026-01-07 15:45:04 +0600

    alhamdulliah read data and find multiple user data and showed data in the ui

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,5 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+
 const Users = () => {
-  return <div>Users</div>;
+  const usersData = useLoaderData();
+  return (
+    <div>
+      <h1>user available : {usersData.length}</h1>
+      <div className="">
+        {usersData.map((item) => (
+          <p key={item._id}>
+            {item.name} : {item.email} : {item._id}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
