69c1dbcc6bfddf9b59da58db4336d12caf188c1b
Taanveer22 <taanveer469@gmail.com>
2026-01-07 14:31:35 +0600

    alhamdulliah load data from client then send to server then send to database

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    //==================== send data from client to server===============
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+  return (
+    <>
+      <h1>Mongodb Client</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="name" /> <br /> <br />
+        <input type="email" name="email" placeholder="email" /> <br /> <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
+  );
+}
+
+export default App;
