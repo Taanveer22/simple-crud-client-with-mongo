8ddb3f6535543bebccb9772d60294f26221a9786
Taanveer22 <taanveer469@gmail.com>
2026-01-07 18:54:46 +0600

    alhamdulliah delete user functionality implementation done

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const usersData = useLoaderData();
+  const [users, setUsers] = useState(usersData);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("user deleted successfully");
+          const remainingUsers = users.filter((element) => element._id !== id);
+          setUsers(remainingUsers);
+        }
+      });
+  };
+  const btnStyle = {
+    backgroundColor: "red",
+    padding: "3px",
+    margin: "6px",
+  };
   return (
     <div>
-      <h1>user available : {usersData.length}</h1>
-      <div className="">
-        {usersData.map((item) => (
+      {/* 1. Use 'users.length' instead of 'usersData.length' */}
+      <h1>user available : {users.length}</h1>
+      <div>
+        {/* 2. Map over 'users' instead of 'usersData' */}
+        {users.map((item) => (
           <p key={item._id}>
             {item.name} : {item.email} : {item._id}
+            <button onClick={() => handleDelete(item._id)} style={btnStyle}>
+              X
+            </button>
           </p>
         ))}
       </div>
